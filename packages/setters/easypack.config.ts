9dc4092373c112df0f251e02cfc7f21e2c16aaa8
JinSooo <kimjinso@qq.com>
2026-01-23 22:05:22 +0200

    feat: use easypack for setters

---
+++ packages/setters/easypack.config.ts
@@ -0,0 +1,8 @@
+/**
+ * @easy-editor/easypack configuration
+ * @type {import('@easy-editor/easypack').EasypackConfig}
+ */
+export default {
+  preset: 'setter',
+  globalName: 'EasyEditorSetters',
+}
